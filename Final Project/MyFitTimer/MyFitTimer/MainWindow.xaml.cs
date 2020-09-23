422afa125c1bbe741cca9119cdd3e86e9c1ef7a0
Allen-SPC <71796034+Allen-SPC@users.noreply.github.com>
2020-09-23 19:00:41 -0400

    Add files via upload

---
+++ Final Project/MyFitTimer/MyFitTimer/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyFitTimer
+{
+   
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void StartTimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Calls StopwatchTracker Class to initiate timer
+
+            //StopwatchTracker stopwatch = new StopwatchTracker();
+            //stopwatch.StartTimer();
+            
+            
+        }
+
+        private void EndTimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Calls StopwatchTracker Class to end timer
+            //StopwatchTracker.StopTimer();
+            
+
+        }
+
+        private void ResetElapsedTimerTextBoxButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Reset elapsed time textbox
+            ElapsedTimeTextBox.Text = "";
+
+
+        }
+
+        private void ResetResultsDataGrid_DB_Click(object sender, RoutedEventArgs e)
+        {
+            //Calls StopwatchTracker Class to reset the db. Resets datagridview also.
+            ResultsDataGrid.DataContext = null;
+
+        }
+    }
+}
